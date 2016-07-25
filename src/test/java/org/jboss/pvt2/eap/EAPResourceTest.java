9c99b0e4f6ddff4397092288a2f49216218a5d18
yyang <yyang@redhat.com>
2016-07-25 12:21:27 +0800

    method order

--- src/test/java/org/jboss/pvt2/eap/EAPResourceTest.java
+++ src/test/java/org/jboss/pvt2/eap/EAPResourceTest.java
@@ -5,6 +5,7 @@ import org.jboss.pvt2.utils.HttpUtils;
 import org.jboss.pvt2.utils.ZipUtils;
 import org.junit.*;
 import org.junit.experimental.categories.Category;
+import org.junit.runners.MethodSorters;
 
 import java.io.File;
 import java.util.logging.Logger;
@@ -15,6 +16,7 @@ import java.util.logging.Logger;
  * Created by yyang on 7/11/16.
  */
 @Category({EAP7.class, EAP6.class})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class EAPResourceTest {
 
     private static Logger logger = PVTLogger.getLogger(EAPResourceTest.class.getName());
@@ -44,14 +46,14 @@ public class EAPResourceTest {
     }
 
     @Test
-    public void testConfiguration(){
+    public void test1Configuration(){
         logger.info(EAP7TestSuite.EAPTestConfig.EAP_DIR_KEY + ": " + EAP7TestSuite.EAPTestConfig.getInstance().getEapDir());
         Assert.assertNotNull(EAP7TestSuite.EAPTestConfig.getInstance().getEapDir());
 
     }
 
     @Test
-    public void testDownload() throws Exception{
+    public void test2Download() throws Exception{
         logger.info("Download EAP ZIP: " + EAP7TestSuite.EAPTestConfig.getInstance().getEapZipUrl());
         HttpUtils.httpDownload(EAP7TestSuite.EAPTestConfig.getInstance().getEapZipUrl(), EAP7TestSuite.getTestConfig().getEapZipName());
         Assert.assertTrue(new File( EAP7TestSuite.EAPTestConfig.getInstance().getEapZipName()).exists());
@@ -59,7 +61,7 @@ public class EAPResourceTest {
     }
 
     @Test
-    public void testExtract() throws Exception{
+    public void test3Extract() throws Exception{
         logger.info("Extract: " + EAP7TestSuite.EAPTestConfig.getInstance().getEapZipName());
         File zipFile = new File( EAP7TestSuite.EAPTestConfig.getInstance().getEapZipName());
         Assert.assertTrue(zipFile.exists());
