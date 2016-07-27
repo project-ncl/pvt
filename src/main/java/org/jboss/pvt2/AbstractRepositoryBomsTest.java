89b1f5690571ef22fc4959de3077f172a6484093
yyang <yyang@redhat.com>
2016-07-27 16:27:27 +0800

    Add 3 test case

---
+++ src/main/java/org/jboss/pvt2/AbstractRepositoryBomsTest.java
@@ -0,0 +1,8 @@
+package org.jboss.pvt2;
+
+/**
+ * Created by yyang on 7/27/16.
+ */
+public abstract class AbstractRepositoryBomsTest {
+    //TODO: https://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/EAP7/view/EAP7-Prod/job/jboss-eap-7.0.x-handoff-check-eap-maven-repository-boms/
+}
