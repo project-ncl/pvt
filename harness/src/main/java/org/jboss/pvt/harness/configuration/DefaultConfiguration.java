fa6336cc676d8d2674edf8c080ad62de0c525f8e
yyang <yyang@redhat.com>
2016-11-04 12:12:18 +0800

    Add testCase Map to Configuration

--- harness/src/main/java/org/jboss/pvt/harness/configuration/DefaultConfiguration.java
+++ harness/src/main/java/org/jboss/pvt/harness/configuration/DefaultConfiguration.java
@@ -126,9 +126,9 @@ public class DefaultConfiguration implements PVTConfiguration
     }
 
     @Override
-    public TestCase getTestCase( String key )
+    public TestCase getTestCase(String key )
     {
-        return config.getTestCase();
+        return config.getTestCases().get(key);
     }
 
     @Override
